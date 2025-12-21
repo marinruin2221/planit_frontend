f7b821cb380fd1eecbe6dae8bfe4cf134f8b9ba9
marinruin <marinruin@naver.com>
2025-12-21 19:46:55 +0900

    메인페이지 국내 인기 여행지 스와이퍼 추가

--- src/components/main/BannerForm.jsx
+++ src/components/main/BannerForm.jsx
@@ -15,22 +15,18 @@ export default function BannerForm()
 	return <React.Fragment>
 		<Swiper effect={"fade"} modules={[EffectFade]}>
 			<SwiperSlide>
-				<Flex pos={"relative"}>
-					<Flex pos={"absolute"} inset={"0"} justify={"center"} align={"normal"} direction={"column"} gap={"5"} p={"20"} bg={"linear-gradient(to right,rgba(0,0,0,0.5),transparent)"}>
-						<Text wordBreak={"keep-all"} fontSize={"7xl"} color={"var(--white_color)"}>여행의 시작은 여기서</Text>
-						<Text wordBreak={"keep-all"} fontSize={"2xl"} color={"var(--white_color)"}>국내부터 해외까지, planit과 함께하세요</Text>
-					</Flex>
-					<Image w={"vw"} h={"vh"} src={"https://static.yeogi.com/_next/static/media/03_Kv_PC_Light_B.fcfed8ce.webp"}/>
+				<Flex pos={"absolute"} inset={"0"} justify={"center"} direction={"column"} p={{base:"4",md:"20"}} bg={{base:"linear-gradient(to bottom,rgba(0,0,0,0.5),transparent)",md:"linear-gradient(to right,rgba(0,0,0,0.5),transparent)"}}>
+					<Text wordBreak={"keep-all"} fontSize={{base:"5xl",md:"7xl"}} color={"var(--white_color)"}>여행의 시작은 여기서</Text>
+					<Text wordBreak={"keep-all"} fontSize={{base:"lg",md:"xl"}} color={"var(--white_color)"}>국내부터 해외까지, planit과 함께하세요</Text>
 				</Flex>
+				<Image w={"vw"} h={"vh"} src={"https://static.yeogi.com/_next/static/media/03_Kv_PC_Light_B.fcfed8ce.webp"}/>
 			</SwiperSlide>
 			<SwiperSlide>
-				<Flex pos={"relative"}>
-					<Flex pos={"absolute"} inset={"0"} justify={"center"} align={"normal"} direction={"column"} gap={"5"} p={"20"} bg={"linear-gradient(to right,rgba(0,0,0,0.5),transparent)"}>
-						<Text wordBreak={"keep-all"} fontSize={"7xl"} color={"var(--white_color)"}>당신만의 일정으로</Text>
-						<Text wordBreak={"keep-all"} fontSize={"2xl"} color={"var(--white_color)"}>숙소부터 일정까지 한 번에 계획하세요</Text>
-					</Flex>
-					<Image w={"vw"} h={"vh"} src={"https://pages.trip.com/Hotels/images/V8HomePageBackGround.webp"}/>
+				<Flex pos={"absolute"} inset={"0"} justify={"center"} direction={"column"} p={{base:"4",md:"20"}} bg={{base:"linear-gradient(to bottom,rgba(0,0,0,0.5),transparent)",md:"linear-gradient(to right,rgba(0,0,0,0.5),transparent)"}}>
+					<Text wordBreak={"keep-all"} fontSize={{base:"5xl",md:"7xl"}} color={"var(--white_color)"}>당신만의 일정으로</Text>
+					<Text wordBreak={"keep-all"} fontSize={{base:"lg",md:"xl"}} color={"var(--white_color)"}>숙소부터 일정까지 한 번에 계획하세요</Text>
 				</Flex>
+				<Image w={"vw"} h={"vh"} src={"https://pages.trip.com/Hotels/images/V8HomePageBackGround.webp"}/>
 			</SwiperSlide>
 		</Swiper>
 	</React.Fragment>
--- src/components/main/BestCard.jsx
+++ src/components/main/BestCard.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Chakra UI
-import { Box , Flex , Text , Button , Card , Icon , Image } from "@chakra-ui/react";
+import { Box , Flex , Text , Button , Image , Icon } from "@chakra-ui/react";
 import { LuArrowRight } from "react-icons/lu";
 
 // Swiper
@@ -12,23 +12,29 @@ import "swiper/css";
 export default function BestCard()
 {
 	return <React.Fragment>
-		<Flex justify={"space-between"} align={"center"} pb={"4"}>
-			<Text fontSize={"2xl"} fontWeight={"bold"}>국내 인기 여행지</Text>
-			<Flex>
-				<Button variant={"plain"} size={"sm"} color={"var(--white_color)"} bg={"var(--brand_color)"} _hover={{bg:"var(--brand_hover_color)"}}>
+		<Flex justify={"space-between"} pb={"4"}>
+			<Box>
+				<Text fontSize={"2xl"} fontWeight={"bold"}>국내 인기 여행지</Text>
+				<Text>지금 안 가면 아쉬운 곳</Text>
+			</Box>
+			<Box>
+				<Button variant={"plain"} size={"xs"} color={"var(--white_color)"} bg={"var(--brand_color)"} _hover={{bg:"var(--brand_hover_color)"}}>
 					<Text>More</Text>
 					<Icon as={LuArrowRight}/>
 				</Button>
-			</Flex>
+			</Box>
 		</Flex>
 		<Swiper slidesPerView={"auto"} spaceBetween={20}>
-			<SwiperSlide style={{width:"200px",height:"200px"}}>
-				<Card.Root pos={"relative"} overflow={"hidden"} cursor={"pointer"}>
-					<Box pos={"absolute"} inset={"auto auto 0 0"} w={"full"} h={"auto"} px={"4"} py={"2"} bg={"linear-gradient(to top,rgba(0,0,0,0.5),transparent)"}>
-						<Text fontSize={"2xl"} fontWeight={"bold"} color={"var(--white_color)"}>서울</Text>
-					</Box>
-					<Image src={"https://image.goodchoice.kr/resize_360x360/exhibition/cms/Region_Seoul_01_20231103164358.png"}/>
-				</Card.Root>
+			<SwiperSlide style={{width:"250px",height:"auto"}}>
+				<Image w={"250px"} h={"200px"} src={"https://image.goodchoice.kr/resize_360x360/exhibition/cms/Region_Seoul_01_20231103164358.png"}/>
+				<Flex direction={"column"} align={"start"} pt={"2"}>
+					<Text fontSize={"md"}>펜션</Text>
+					<Text fontSize={"xl"}>제주 펜션 1호점</Text>
+				</Flex>
+				<Flex direction={"column"} align={"end"} pt={"2"}>
+					<Text fontSize={"md"}>80,000원</Text>
+					<Text fontSize={"xl"}>23,000원</Text>
+				</Flex>
 			</SwiperSlide>
 		</Swiper>
 	</React.Fragment>
