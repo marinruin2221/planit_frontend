aea7f205aecd788b58d00d853b48641df2ed1b68
jjonghyunee <a01062327136z@gmail.com>
2025-12-22 12:32:21 +0900

    이벤트 페이지 디자인 수정

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -14,84 +14,84 @@ export default function EventPage()
         {
             id: 1,
             title: "겨울 특별전",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5502_ghaqosj_1200x480_20251126145213.png",
+            image: "https://image6.yanolja.com/cx-ydm/kzYJoRc7Eo9itmL8",
             period: "25.12.01 - 25.12.31",
             category: "모텔"
         },
         {
             id: 2,
             title: "신년 맞이 할인",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5517_dnpqqosj_1200x480_20251128140533.png",
+            image: "https://image6.yanolja.com/cx-ydm/qQ2a8GlG3dDs2sv0",
             period: "25.12.01 - 25.12.31",
             category: "호텔리조트"
         },
         {
             id: 3,
             title: "멤버십 데이",
-            image: "https://image.goodchoice.kr/exhibition/cms/dnpqqosj_1200x480_flsbdjf_20251203100902.png",
+            image: "https://image6.yanolja.com/cx-ydm/gfgPQFZbAVMKIUQD",
             period: "25.12.08 - 25.12.14",
             category: "펜션·캠핑·게하"
         },
         {
             id: 4,
             title: "겨울 특별전",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5502_ghaqosj_1200x480_20251126145213.png",
+            image: "https://image6.yanolja.com/cx-ydm/Tv7mRL1cDUvQlWxD",
             period: "25.12.01 - 25.12.31",
             category: "모텔"
         },
         {
             id: 5,
             title: "신년 맞이 할인",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5517_dnpqqosj_1200x480_20251128140533.png",
+            image: "https://image6.yanolja.com/cx-ydm/3Ehmj6Xymtn4lfQx",
             period: "25.12.01 - 25.12.31",
             category: "공간대여"
         },
         {
             id: 6,
             title: "멤버십 데이",
-            image: "https://image.goodchoice.kr/exhibition/cms/dnpqqosj_1200x480_flsbdjf_20251203100902.png",
+            image: "https://image6.yanolja.com/cx-ydm/duh8D212NSjf1Q1K",
             period: "25.12.08 - 25.12.14",
             category: "호텔리조트"
         },
         {
             id: 7,
             title: "겨울 특별전",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5502_ghaqosj_1200x480_20251126145213.png",
+            image: "https://image6.yanolja.com/cx-ydm/jeVrxenjd1xR137u",
             period: "25.12.01 - 25.12.31",
             category: "모텔"
         },
         {
             id: 8,
             title: "신년 맞이 할인",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5517_dnpqqosj_1200x480_20251128140533.png",
+            image: "https://image6.yanolja.com/cx-ydm/CsCRwwdDz9kO2WF0",
             period: "25.12.01 - 25.12.31",
             category: "호텔리조트"
         },
         {
             id: 9,
             title: "멤버십 데이",
-            image: "https://image.goodchoice.kr/exhibition/cms/dnpqqosj_1200x480_flsbdjf_20251203100902.png",
+            image: "https://image6.yanolja.com/cx-ydm/KGGPSZgFdjMdmVKL",
             period: "25.12.08 - 25.12.14",
             category: "펜션·캠핑·게하"
         },
         {
             id: 10,
             title: "겨울 특별전",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5502_ghaqosj_1200x480_20251126145213.png",
+            image: "https://image6.yanolja.com/cx-ydm/PrAdVg6gNOTXh4Ie",
             period: "25.12.01 - 25.12.31",
             category: "모텔"
         },
         {
             id: 11,
             title: "신년 맞이 할인",
-            image: "https://image.goodchoice.kr/exhibition/cms/e5517_dnpqqosj_1200x480_20251128140533.png",
+            image: "https://image6.yanolja.com/cx-ydm/MAh7l0Is8geJ6kOQ",
             period: "25.12.01 - 25.12.31",
             category: "공간대여"
         },
         {
             id: 12,
             title: "멤버십 데이",
-            image: "https://image.goodchoice.kr/exhibition/cms/dnpqqosj_1200x480_flsbdjf_20251203100902.png",
+            image: "https://image6.yanolja.com/cx-ydm/5B72JloE6fLwYMzG",
             period: "25.12.08 - 25.12.14",
             category: "호텔리조트"
         }
@@ -226,19 +226,19 @@ export default function EventPage()
                         {pagedEvents.map((e) => (
                             <Box
                                 key={e.id}
-                                borderWidth="1px"
+                                // borderWidth="1px"
                                 borderRadius="lg"
                                 overflow="hidden"
-                                _hover={{ boxShadow: "lg", transform: "scale(1.02)", bgColor:"#dd6b20" }}
+                                _hover={{ transform: "scale(1.05)" }}
                                 transition="all 0.2s"
                             >
                                 <Image src={e.image} alt={e.title} />
 
                                 <Box p={4}>
-                                    <Text fontSize="xl" fontWeight="semibold">
+                                    {/* <Text fontSize="xl" fontWeight="semibold">
                                         {e.title}
-                                    </Text>
-                                    <Text mt={2} fontSize="sm" color="gray.600">
+                                    </Text> */}
+                                    <Text mt={0} fontSize="sm" color="gray.600">
                                         {e.period}
                                     </Text>
                                 </Box>
