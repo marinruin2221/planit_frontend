ae4fcf15d2a49ca4cf9c87af8e809c7c0c4759a4
marinruin2221 <marinruin@naver.com>
2025-12-23 02:21:10 +0900

    1. 메인페이지 광고 배너 추가

--- src/components/main/AreaCard.jsx
+++ src/components/main/AreaCard.jsx
@@ -7,31 +7,32 @@ import { LuArrowRight } from "react-icons/lu";
 
 // Swiper
 import { Swiper , SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/free-mode";
 
-export default function AreaCard({name,more,data})
+export default function AreaCard({name,data})
 {
 	return <React.Fragment>
 		<Flex justify={"space-between"} align={"center"} pb={"4"}>
 			<Box>
 				<Text fontSize={"2xl"} fontWeight={"bold"}>{name}</Text>
 			</Box>
-			{more && (
 			<Box>
 				<Button variant={"plain"} size={"xs"} color={"var(--white_color)"} bg={"var(--brand_color)"} _hover={{bg:"var(--brand_hover_color)"}}>
 					<Text>More</Text>
 					<Icon as={LuArrowRight}/>
 				</Button>
 			</Box>
-			)}
 		</Flex>
-		<Swiper slidesPerView={"auto"} spaceBetween={"20"}>
+		<Swiper modules={[FreeMode]} freeMode={true} slidesPerView={"auto"} spaceBetween={"20"}>
 			{data.map((e)=>(
 			<SwiperSlide style={{width:"250px",height:"auto"}}>
 				<Box cursor={"pointer"}>
-					<Image w={"250px"} h={"350px"} rounded={"md"} src={e.image}/>
-					<Box>
-						<Text>{e.name}</Text>
+					<Image w={"250px"} h={"350px"} rounded={"md"} filter={"brightness(0.5)"} src={e.image}/>
+					<Box pos={"absolute"} inset={"auto 0 0 0"} p={"4"}>
+						<Text fontSize={"xl"} color={"var(--white_color)"}>{e.subject}</Text>
+						<Text fontSize={"sm"} color={"var(--white_color)"}>{e.content}</Text>
 					</Box>
 				</Box>
 			</SwiperSlide>
--- src/components/main/StayCard.jsx
+++ src/components/main/StayCard.jsx
@@ -8,34 +8,34 @@ import { FaStar } from "react-icons/fa";
 
 // Swiper
 import { Swiper , SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/free-mode";
 
-export default function StayCard({name,more,data})
+export default function StayCard({name,data})
 {
 	return <React.Fragment>
 		<Flex justify={"space-between"} align={"center"} pb={"4"}>
 			<Box>
 				<Text fontSize={"2xl"} fontWeight={"bold"}>{name}</Text>
 			</Box>
-			{more && (
 			<Box>
 				<Button variant={"plain"} size={"xs"} color={"var(--white_color)"} bg={"var(--brand_color)"} _hover={{bg:"var(--brand_hover_color)"}}>
 					<Text>More</Text>
 					<Icon as={LuArrowRight}/>
 				</Button>
 			</Box>
-			)}
 		</Flex>
-		<Swiper slidesPerView={"auto"} spaceBetween={"20"}>
+		<Swiper modules={[FreeMode]} freeMode={true} slidesPerView={"auto"} spaceBetween={"20"}>
 			{data.map((e)=>(
 			<SwiperSlide style={{width:"250px",height:"auto"}}>
 				<Box cursor={"pointer"}>
 					<Image w={"250px"} h={"150px"} rounded={"md"} src={e.image}/>
-					<Box pt={"2"}>
-						<Text truncate textAlign={"start"} fontSize={"md"} color={"var(--brand_color)"}>{e.type}</Text>
+					<Box pt={"4"}>
+						<Text truncate textAlign={"start"} fontSize={"sm"} color={"var(--brand_color)"}>{e.type}</Text>
 						<Text truncate textAlign={"start"} fontSize={"xl"} color={"var(--black_color)"}>{e.name}</Text>
 					</Box>
-					<Box pt={"2"}>
+					<Box pt={"4"}>
 						<Flex justify={"space-between"} align={"end"}>
 							<Flex gap={"1"}>
 								<Icon color={"var(--star_color)"} as={FaStar}/>
@@ -43,7 +43,7 @@ export default function StayCard({name,more,data})
 								<Text color={"var(--text_light_gray)"}>({e.review.toLocaleString()})</Text>
 							</Flex>
 							<Box>
-								<Text truncate textAlign={"end"} fontSize={"md"} color={"var(--text_light_gray)"} textDecor={"line-through"}>{e.basePrice.toLocaleString()}원</Text>
+								<Text truncate textAlign={"end"} fontSize={"sm"} color={"var(--text_light_gray)"} textDecor={"line-through"}>{e.basePrice.toLocaleString()}원</Text>
 								<Text truncate textAlign={"end"} fontSize={"xl"} color={"var(--brand_color)"}>{e.salePrice.toLocaleString()}원</Text>
 							</Box>
 						</Flex>
